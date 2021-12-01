e1d44ea5346f0cbe1b9f8f8fa54b25aa9c3c51f4
John Downey <jdowney@gmail.com>
2021-12-01 05:11:53 -0600

    Day 1

---
+++ src/day1.rs
@@ -0,0 +1,23 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day1)]
+fn generator(input: &str) -> Vec<u32> {
+    input.lines().map(str::parse).map(Result::unwrap).collect()
+}
+
+#[aoc(day1, part1)]
+fn part1(input: &[u32]) -> usize {
+    input
+        .windows(2)
+        .filter(|readings| readings[1] > readings[0])
+        .count()
+}
+
+#[aoc(day1, part2)]
+fn part2(input: &[u32]) -> usize {
+    let sums: Vec<u32> = input
+        .windows(3)
+        .map(|readings| readings.iter().sum())
+        .collect();
+    sums.windows(2).filter(|parts| parts[1] > parts[0]).count()
+}

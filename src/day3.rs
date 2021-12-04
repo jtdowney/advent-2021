c42a96d7b7b0d7159c81a982bacf6336700209bc
John Downey <jdowney@gmail.com>
2021-12-03 20:07:30 -0600

    Day 3 improvements

--- src/day3.rs
+++ src/day3.rs
@@ -1,28 +1,40 @@
 use itertools::Itertools;
+use std::collections::HashMap;
 
 #[aoc_generator(day3)]
-fn generator(input: &str) -> Vec<Vec<char>> {
-    input.lines().map(|line| line.chars().collect()).collect()
+fn generator(input: &str) -> Vec<String> {
+    input.lines().map(String::from).collect()
 }
 
 #[aoc(day3, part1)]
-fn part1(input: &[Vec<char>]) -> eyre::Result<u32> {
-    let digits = input[0].len();
+fn part1(input: &[String]) -> eyre::Result<u32> {
+    let input = (0..)
+        .map_while(|i| {
+            let counts = input
+                .iter()
+                .map(|line| line.chars().nth(i))
+                .collect::<Option<Vec<char>>>()?
+                .into_iter()
+                .counts();
 
-    let gamma = (0..digits)
-        .map(|i| {
-            let counts = input.iter().map(|line| line[i]).counts();
-            let (&digit, _) = counts.iter().max_by_key(|(_, &v)| v).unwrap();
-            digit
+            Some(counts)
+        })
+        .collect::<Vec<HashMap<char, usize>>>();
+
+    let gamma = input
+        .iter()
+        .map(|counts| {
+            let (&value, _) = counts.iter().max_by_key(|(_, &v)| v).unwrap();
+            value
         })
         .collect::<String>();
     let gamma = u32::from_str_radix(&gamma, 2)?;
 
-    let epsilon = (0..digits)
-        .map(|i| {
-            let counts = input.iter().map(|line| line[i]).counts();
-            let (&digit, _) = counts.iter().min_by_key(|(_, &v)| v).unwrap();
-            digit
+    let epsilon = input
+        .iter()
+        .map(|counts| {
+            let (&value, _) = counts.iter().min_by_key(|(_, &v)| v).unwrap();
+            value
         })
         .collect::<String>();
     let epsilon = u32::from_str_radix(&epsilon, 2)?;
@@ -31,47 +43,52 @@ fn part1(input: &[Vec<char>]) -> eyre::Result<u32> {
 }
 
 #[aoc(day3, part2)]
-fn part2(input: &[Vec<char>]) -> eyre::Result<u32> {
-    let mut entries = input.to_vec();
-    let mut digit = 0;
-    loop {
-        if entries.len() == 1 {
-            break;
-        }
+fn part2(input: &[String]) -> eyre::Result<u32> {
+    let input = input
+        .iter()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
 
-        let counts = entries.iter().map(|line| line[digit]).counts();
-        let pick = if counts[&'0'] == counts[&'1'] {
-            '1'
-        } else {
-            let (&pick, _) = counts.iter().max_by_key(|(_, &v)| v).unwrap();
-            pick
-        };
+    let generator = (0..)
+        .try_fold(input.clone(), |mut candidates, digit| {
+            if candidates.len() == 1 {
+                let value = candidates[0].iter().collect::<String>();
+                return Err(value);
+            }
 
-        entries.retain(|entry| entry[digit] == pick);
-        digit += 1;
-    }
-    let generator = entries[0].iter().collect::<String>();
+            let counts = candidates.iter().map(|line| line[digit]).counts();
+            let pick = if counts[&'0'] == counts[&'1'] {
+                '1'
+            } else {
+                let (&pick, _) = counts.iter().max_by_key(|(_, &v)| v).unwrap();
+                pick
+            };
+            candidates.retain(|candidate| candidate[digit] == pick);
+
+            Ok(candidates)
+        })
+        .unwrap_err();
     let generator = u32::from_str_radix(&generator, 2)?;
 
-    let mut entries = input.to_vec();
-    let mut digit = 0;
-    loop {
-        if entries.len() == 1 {
-            break;
-        }
+    let scrubber = (0..)
+        .try_fold(input.clone(), |mut candidates, digit| {
+            if candidates.len() == 1 {
+                let value = candidates[0].iter().collect::<String>();
+                return Err(value);
+            }
 
-        let counts = entries.iter().map(|line| line[digit]).counts();
-        let pick = if counts[&'0'] == counts[&'1'] {
-            '0'
-        } else {
-            let (&pick, _) = counts.iter().min_by_key(|(_, &v)| v).unwrap();
-            pick
-        };
+            let counts = candidates.iter().map(|line| line[digit]).counts();
+            let pick = if counts[&'0'] == counts[&'1'] {
+                '0'
+            } else {
+                let (&pick, _) = counts.iter().min_by_key(|(_, &v)| v).unwrap();
+                pick
+            };
+            candidates.retain(|candidate| candidate[digit] == pick);
 
-        entries.retain(|entry| entry[digit] == pick);
-        digit += 1;
-    }
-    let scrubber = entries[0].iter().collect::<String>();
+            Ok(candidates)
+        })
+        .unwrap_err();
     let scrubber = u32::from_str_radix(&scrubber, 2)?;
 
     Ok(generator * scrubber)

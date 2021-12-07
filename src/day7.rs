954ec23faaf334c30b31452175d0c7367c774008
John Downey <jdowney@gmail.com>
2021-12-07 05:08:35 -0600

    Day 7 cleanup

--- src/day7.rs
+++ src/day7.rs
@@ -12,31 +12,32 @@ fn generator(input: &str) -> eyre::Result<Vec<i32>> {
         .collect()
 }
 
-#[aoc(day7, part1)]
-fn part1(input: &[i32]) -> Option<i32> {
+fn solve<F>(input: &[i32], cost: F) -> Option<i32>
+where
+    F: Fn(i32, i32) -> i32,
+{
     let (&start, &end) = input.iter().minmax().into_option()?;
     (start..=end)
         .map(|destination| {
             input
                 .iter()
-                .map(|&position| (position - destination).abs())
+                .map(|&position| cost(destination, position))
                 .sum()
         })
         .min()
 }
 
+#[aoc(day7, part1)]
+fn part1(input: &[i32]) -> Option<i32> {
+    solve(input, |destination, position| {
+        (position - destination).abs()
+    })
+}
+
 #[aoc(day7, part2)]
 fn part2(input: &[i32]) -> Option<i32> {
-    let (&start, &end) = input.iter().minmax().into_option()?;
-    (start..=end)
-        .map(|destination| {
-            input
-                .iter()
-                .map(|&position| {
-                    let steps = (position - destination).abs();
-                    (1..=steps).sum::<i32>()
-                })
-                .sum()
-        })
-        .min()
+    solve(input, |destination, position| {
+        let steps = (position - destination).abs();
+        (1..=steps).sum::<i32>()
+    })
 }

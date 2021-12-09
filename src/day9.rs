44615b5f771b8ddd670d623f364227c544f1e151
John Downey <jdowney@gmail.com>
2021-12-09 10:15:42 -0600

    Day 9 cleanup

--- src/day9.rs
+++ src/day9.rs
@@ -2,8 +2,6 @@ use std::collections::{HashMap, HashSet, VecDeque};
 
 type Point = (i16, i16);
 
-const NEIGHBOR_DELTAS: [Point; 4] = [(0, -1), (-1, 0), (1, 0), (0, 1)];
-
 #[aoc_generator(day9)]
 fn generator(input: &str) -> Option<HashMap<Point, u16>> {
     input
@@ -17,13 +15,18 @@ fn generator(input: &str) -> Option<HashMap<Point, u16>> {
         .collect()
 }
 
+fn neighbors((x, y): Point) -> impl Iterator<Item = Point> {
+    [(0, -1), (-1, 0), (1, 0), (0, 1)]
+        .into_iter()
+        .map(move |(dx, dy)| (x + dx, y + dy))
+}
+
 fn low_points(input: &HashMap<Point, u16>) -> impl Iterator<Item = Point> + '_ {
     input
         .iter()
-        .filter(|&(&(x, y), &height)| {
-            NEIGHBOR_DELTAS
-                .into_iter()
-                .filter_map(|(dx, dy)| input.get(&(x + dx, y + dy)))
+        .filter(|&(&point, &height)| {
+            neighbors(point)
+                .filter_map(|neighbor| input.get(&neighbor))
                 .all(|&neighbor_height| neighbor_height > height)
         })
         .map(|(&point, _)| point)
@@ -34,30 +37,22 @@ fn basin_size(start: Point, input: &HashMap<Point, u16>) -> usize {
     let mut search = VecDeque::new();
     search.push_back(start);
 
-    while let Some(point @ (x, y)) = search.pop_front() {
+    while let Some(point) = search.pop_front() {
         visited.insert(point);
 
-        let neighbors = NEIGHBOR_DELTAS
-            .into_iter()
-            .map(|(dx, dy)| {
-                let nx = x + dx;
-                let ny = y + dy;
-                (nx, ny)
-            })
-            .filter(|neighbor| {
-                if visited.contains(neighbor) {
-                    return false;
-                }
+        let basin_neighbors = neighbors(point).filter(|neighbor| {
+            if visited.contains(neighbor) {
+                return false;
+            }
 
-                if let Some(&height) = input.get(neighbor) {
-                    height < 9
-                } else {
-                    false
-                }
-            })
-            .collect::<Vec<Point>>();
+            if let Some(&height) = input.get(neighbor) {
+                height < 9
+            } else {
+                false
+            }
+        });
 
-        search.extend(neighbors);
+        search.extend(basin_neighbors);
     }
 
     visited.len()
@@ -65,7 +60,7 @@ fn basin_size(start: Point, input: &HashMap<Point, u16>) -> usize {
 
 #[aoc(day9, part1)]
 fn part1(input: &HashMap<Point, u16>) -> u16 {
-    low_points(&input)
+    low_points(input)
         .filter_map(|point| input.get(&point))
         .map(|&height| height + 1)
         .sum()
@@ -73,10 +68,10 @@ fn part1(input: &HashMap<Point, u16>) -> u16 {
 
 #[aoc(day9, part2)]
 fn part2(input: &HashMap<Point, u16>) -> usize {
-    let mut basin_sizes = low_points(&input)
+    let mut basin_sizes = low_points(input)
         .map(|point| basin_size(point, input))
         .collect::<Vec<usize>>();
 
     basin_sizes.sort_unstable();
-    basin_sizes.iter().rev().take(3).copied().product()
+    basin_sizes.into_iter().rev().take(3).product()
 }

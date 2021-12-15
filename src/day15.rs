6ba0605792a64f366c30c899b3f36b3cba6950d8
John Downey <jdowney@gmail.com>
2021-12-15 08:08:59 -0600

    Day 15 cleanups

--- src/day15.rs
+++ src/day15.rs
@@ -1,19 +1,19 @@
 use std::{
-    cmp::{Ordering, Reverse},
+    cmp::Ordering,
     collections::{BinaryHeap, HashMap},
 };
 
 type Point = (i16, i16);
 
 #[aoc_generator(day15)]
-fn generator(input: &str) -> Option<HashMap<Point, u8>> {
+fn generator(input: &str) -> Option<HashMap<Point, u32>> {
     input
         .lines()
         .enumerate()
         .flat_map(|(y, line)| {
             line.chars()
                 .enumerate()
-                .map(move |(x, c)| c.to_digit(10).map(|n| ((x as i16, y as i16), n as u8)))
+                .map(move |(x, c)| c.to_digit(10).map(|n| ((x as i16, y as i16), n)))
         })
         .collect()
 }
@@ -27,63 +27,56 @@ fn neighbors((x, y): Point) -> impl Iterator<Item = Point> {
 #[derive(PartialEq, Eq, Clone, Copy)]
 struct SearchEntry {
     point: Point,
-    score: u32,
+    risk: u32,
 }
 
 impl PartialOrd for SearchEntry {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.score.partial_cmp(&other.score)
+        self.risk.partial_cmp(&other.risk).map(|o| o.reverse())
     }
 }
 
 impl Ord for SearchEntry {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.score.cmp(&other.score)
+        self.risk.cmp(&other.risk).reverse()
     }
 }
 
-fn search(grid: &HashMap<Point, u8>) -> u32 {
+fn search(grid: &HashMap<Point, u32>) -> u32 {
     let goal = grid.keys().max().cloned().unwrap();
-    let mut cost = HashMap::new();
-    cost.insert((0, 0), 0);
+
+    let mut total_risk = HashMap::new();
+    total_risk.insert((0, 0), 0);
 
     let mut search = BinaryHeap::new();
-    search.push(Reverse(SearchEntry {
+    search.push(SearchEntry {
         point: (0, 0),
-        score: 0,
-    }));
+        risk: 0,
+    });
 
-    while let Some(Reverse(entry)) = search.pop() {
+    while let Some(entry) = search.pop() {
         if entry.point == goal {
             break;
         }
 
         neighbors(entry.point)
-            .filter(|n| grid.contains_key(n))
-            .for_each(|neighbor| {
-                if !cost.contains_key(&neighbor)
-                    || cost[&entry.point] + (grid[&neighbor] as u32) < cost[&neighbor]
+            .filter(|next| grid.contains_key(next))
+            .for_each(|next| {
+                if !total_risk.contains_key(&next)
+                    || total_risk[&entry.point] + grid[&next] < total_risk[&next]
                 {
-                    let distance = manhatten(neighbor, goal);
-                    let neighbor_cost = cost[&entry.point] + grid[&neighbor] as u32;
-                    cost.insert(neighbor, neighbor_cost);
+                    let risk = total_risk[&entry.point] + grid[&next];
+                    total_risk.insert(next, risk);
 
-                    search.push(Reverse(SearchEntry {
-                        point: neighbor,
-                        score: neighbor_cost + distance,
-                    }));
+                    search.push(SearchEntry { point: next, risk });
                 }
             })
     }
 
-    cost[&goal]
+    total_risk[&goal]
 }
 
-fn manhatten((x, y): Point, (gx, gy): Point) -> u32 {
-    ((x - gx).abs() + (y - gy).abs()) as u32
-}
-
-fn expand(grid: &HashMap<Point, u8>) -> HashMap<Point, u8> {
+fn expand(grid: &HashMap<Point, u32>) -> HashMap<Point, u32> {
     let (endx, endy) = grid.keys().max().cloned().unwrap();
     let width = endx + 1;
     let height = endy + 1;
@@ -104,12 +97,12 @@ fn expand(grid: &HashMap<Point, u8>) -> HashMap<Point, u8> {
 }
 
 #[aoc(day15, part1)]
-fn part1(input: &HashMap<Point, u8>) -> u32 {
+fn part1(input: &HashMap<Point, u32>) -> u32 {
     search(input)
 }
 
 #[aoc(day15, part2)]
-fn part2(input: &HashMap<Point, u8>) -> u32 {
+fn part2(input: &HashMap<Point, u32>) -> u32 {
     let grid = expand(input);
     search(&grid)
 }
